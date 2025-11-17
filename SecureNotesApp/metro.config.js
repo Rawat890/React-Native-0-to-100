4ff73e122c5232603ed3017941c92f54925f3886
Rawat890 <rohanrawat7508@gmail.com>
2025-11-17 11:33:23 +0530

    chore: file updated for svg

--- SecureNotesApp/metro.config.js
+++ SecureNotesApp/metro.config.js
@@ -1,11 +1,20 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Remove .svg from assetExts
+defaultConfig.resolver.assetExts =
+  defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg');
+
+// Add .svg to sourceExts
+defaultConfig.resolver.sourceExts.push('svg');
+
+// Use the SVG transformer
+defaultConfig.transformer = {
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+module.exports = mergeConfig(defaultConfig, {});
