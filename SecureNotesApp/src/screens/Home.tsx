8224b2c933bc6635f38e3e1c32d502e2ff350e64
Rawat890 <rohanrawat7508@gmail.com>
2025-11-17 12:26:27 +0530

    chore: Home screen updated

--- SecureNotesApp/src/screens/Home.tsx
+++ SecureNotesApp/src/screens/Home.tsx
@@ -1,10 +1,138 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import DocumentIcon from '../assets/icons/document.svg';
+import PersonIcon from '../assets/icons/person.svg';
+import ShieldIcon from '../assets/icons/shield.svg';
+import CloudIcon from '../assets/icons/cloud.svg';
+import LockIcon from '../assets/icons/lock.svg';
+
+import { FONT_SIZE, FONT_WEIGHT } from '../utils/others';
+import { COLORS } from '../utils/colors';
+import { scale } from 'react-native-size-matters';
+import TextButton from '../components/TextButton';
+import { navigate } from '../utils/NavigationService';
+import { SCREENS } from '../utils/navigation';
 
 export default function Home() {
+  const navigateToSlider = () =>{
+    navigate(SCREENS.AppSlider)
+  }
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.primaryText}>
+        Welcome to <Text style={styles.secondaryText}>Secure Notes</Text>
+      </Text>
+
+      <Text style={styles.normalText}>
+        Great new tools for notes with secure encryption
+      </Text>
+
+      <View style={styles.infoContainer}>
+
+        {/* Document Section */}
+        <View style={styles.innerContainer}>
+          <DocumentIcon width={60} height={60} />
+          <View>
+            <Text style={styles.titleText}>Add almost anything</Text>
+            <Text style={styles.description}>Sketch your thoughts, write your notes</Text>
+          </View>
+        </View>
+
+        {/* Person Section */}
+        <View style={styles.innerContainer}>
+          <PersonIcon width={60} height={60} />
+          <View>
+            <Text style={styles.titleText}>Made for everyone</Text>
+            <Text style={styles.description}>
+              Easy-to-use even for beginners
+            </Text>
+          </View>
+        </View>
+
+        {/* Shield Section */}
+        <View style={styles.innerContainer}>
+          <ShieldIcon width={60} height={60} />
+          <View>
+            <Text style={styles.titleText}>Strong Security</Text>
+            <Text style={styles.description}>
+              Your notes are encrypted and safe
+            </Text>
+          </View>
+        </View>
+
+        {/* Cloud Section */}
+        <View style={styles.innerContainer}>
+          <CloudIcon width={60} height={60} />
+          <View>
+            <Text style={styles.titleText}>Sync Across Devices</Text>
+            <Text style={styles.description}>
+              Access your notes from anywhere
+            </Text>
+          </View>
+        </View>
+
+        {/* Lock Section */}
+        <View style={styles.innerContainer}>
+          <LockIcon width={60} height={60} />
+          <View>
+            <Text style={styles.titleText}>Private by Design</Text>
+            <Text style={styles.description}>
+              Only you can unlock your notes
+            </Text>
+          </View>
+        </View>
+
+      </View>
+      
+      <View style={styles.buttonContainer}>
+      <TextButton size={'medium'} onPress={navigateToSlider}>Continue</TextButton>
+      </View>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: scale(15),
+    paddingTop: scale(30),
+    backgroundColor: COLORS.white,
+  },
+  primaryText: {
+    fontSize: FONT_SIZE['2xl'],
+    textAlign: 'center',
+  },
+  secondaryText: {
+    fontSize: FONT_SIZE['2xl'],
+    color: COLORS.purple,
+    fontWeight: FONT_WEIGHT.bold,
+  },
+  normalText: {
+    fontSize: FONT_SIZE.lg,
+    marginTop: scale(10),
+    textAlign: 'center',
+    color: COLORS.gray,
+  },
+  infoContainer: {
+    marginTop: scale(30),
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    gap: scale(20),
+    paddingVertical: scale(15),
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: FONT_SIZE.lg,
+    fontWeight: FONT_WEIGHT.medium,
+    color: COLORS.black,
+  },
+  description: {
+    fontSize: FONT_SIZE.md,
+    color: COLORS.black,
+    marginTop: scale(5),
+  },
+  buttonContainer:{
+    marginTop: scale(60)
+  }
+});
---
+++ SecureNotesApp/src/utils/others.tsx
@@ -0,0 +1,42 @@
+export const FONT_SIZE = {
+  xs: 10,
+  sm: 12,
+  md: 14,
+  base: 16,
+  lg: 18,
+  xl: 20,
+  "2xl": 24,
+  "3xl": 30,
+  "4xl": 36,
+} as const;
+
+export const FONT_WEIGHT = {
+  thin: "100",
+  extraLight: "200",
+  light: "300",
+  normal: "400",
+  medium: "500",
+  semibold: "600",
+  bold: "700",
+  extraBold: "800",
+  black: "900",
+} as const;
+
+export const SPACING = {
+  xs: 4,
+  sm: 8,
+  md: 12,
+  lg: 16,
+  xl: 20,
+  "2xl": 24,
+  "3xl": 32,
+  "4xl": 40,
+} as const;
+
+export const RADIUS = {
+  sm: 4,
+  md: 8,
+  lg: 12,
+  xl: 16,
+  full: 9999,
+} as const;
