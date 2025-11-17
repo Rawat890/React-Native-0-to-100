9fadc7b66c3e24c36e55b593996678668e42a777
Rawat890 <rohanrawat7508@gmail.com>
2025-11-17 12:26:48 +0530

    chore: safe area view added

--- SecureNotesApp/App.tsx
+++ SecureNotesApp/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import AppNavigator from './src/navigation/AppNavigator';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
   return (
-    <>
+    <SafeAreaProvider>
+    <SafeAreaView style={{ flex: 1 }}>
     <AppNavigator/>
-    </>
+    </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
 
