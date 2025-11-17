535b1a1fb21b864684eb27d076857d9066ae6b1a
Rawat890 <rohanrawat7508@gmail.com>
2025-11-17 14:50:42 +0530

    chore: gesture handler import added

--- SecureNotesApp/App.tsx
+++ SecureNotesApp/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AppNavigator from './src/navigation/AppNavigator';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler'
 
 function App() {
   return (
