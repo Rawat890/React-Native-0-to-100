dff7e17010ba943854266d4071bd31b6e81c4981
Rawat890 <rohanrawat7508@gmail.com>
2025-11-26 07:33:25 +0530

    chore: file updated

--- SecureNotesApp/src/navigation/AppNavigator.tsx
+++ SecureNotesApp/src/navigation/AppNavigator.tsx
@@ -18,8 +18,8 @@ export default function AppNavigator({initialRoute}: AppNavigatorProps) {
   return (
     <NavigationContainer ref={navigationRef}>
     <Stack.Navigator initialRouteName={initialRoute} screenOptions={{headerShown: false}}>
-      <Stack.Screen name={SCREENS.Home} component={Home}/>
       <Stack.Screen name={SCREENS.AppSlider} component={AppSlider}/>
+      <Stack.Screen name={SCREENS.Home} component={Home}/>
       <Stack.Screen name={SCREENS.AddNotes} component={AddNotes}/>
       <Stack.Screen name={SCREENS.ViewNotes} component={ViewNotes}/>
     </Stack.Navigator>
